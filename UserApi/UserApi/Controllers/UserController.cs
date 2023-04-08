3f5a1b4766e6dfdaefba92dbaae86db5bf1f7bc6
Leonardo Moreno <leonardomoreno@alunos.utfpr.edu.br>
2023-04-07 22:51:12 -0300

    Initial

---
+++ UserApi/UserApi/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UserApi.Models;
+
+namespace UserApi.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase {
+        [HttpGet]
+        public ActionResult<List<UserModel>> SearchAllUsers() {
+            return Ok();
+        }
+    }
+}
