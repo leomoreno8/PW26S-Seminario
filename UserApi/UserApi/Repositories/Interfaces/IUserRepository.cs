3fd14a944a8ca6965a59308cd5350846168e229d
Leonardo Moreno <leonardomoreno@alunos.utfpr.edu.br>
2023-04-09 01:50:01 -0300

    update

--- UserApi/UserApi/Repositories/Interfaces/IUserRepository.cs
+++ UserApi/UserApi/Repositories/Interfaces/IUserRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace UserApi.Repositories.Interfaces
+﻿using UserApi.Models;
+
+namespace UserApi.Repositories.Interfaces
 {
-    public interface IUserRepository
-    {
+    public interface IUserRepository {
+        Task<List<UserModel>> SearchAllUsers();
+        Task<UserModel> SearchUserById(int id);
+        Task<UserModel> AddUser(UserModel user);
+        Task<UserModel> UpdateUser(UserModel user, int id);
+        Task<bool> DeleteUser(int id);
     }
 }
