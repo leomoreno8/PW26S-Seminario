8206c39fa35922bcf7596e2335ef2caa3f48293d
Leonardo Moreno <leonardomoreno@alunos.utfpr.edu.br>
2023-04-09 21:05:13 -0300

    migration

--- UserApi/UserApi/Controllers/UserController.cs
+++ UserApi/UserApi/Controllers/UserController.cs
@@ -1,14 +1,35 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using UserApi.Models;
+using UserApi.Repositories.Interfaces;
 
 namespace UserApi.Controllers {
     [Route("api/[controller]")]
     [ApiController]
     public class UserController : ControllerBase {
+        private readonly IUserRepository _userRepository;
+        public UserController(IUserRepository userRepository) {
+            _userRepository = userRepository;
+        }
+
         [HttpGet]
-        public ActionResult<List<UserModel>> SearchAllUsers() {
-            return Ok();
+        public async Task<ActionResult<List<UserModel>>> SearchAllUsers() {
+            List<UserModel> users = await _userRepository.SearchAllUsers();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserModel>> SearchUserById(int id)
+        {
+            UserModel user = await _userRepository.SearchUserById(id);
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<UserModel>> AddUser([FromBody] UserModel userModel)
+        {
+            UserModel user = await _userRepository.AddUser(userModel);
+            return Ok(user);
         }
     }
 }
